35e900910aae2f900882f7c2c21c94f6eeceec19
YastrebovaAnna <ipz221_yaav@student.ztu.edu.ua>
2022-11-11 19:59:58 +0200

    Changes2line

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -3,5 +3,5 @@ Console.WriteLine("Yastrebova Anna");
 Console.WriteLine("Yastrebova Anna ");
 Console.WriteLine("IPZ-22-1 ONE LOVE");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Yastrebova Anna IPZ-22-1(2)");
+Console.WriteLine("Yastrebova Anna IPZ-22-1(2)");
